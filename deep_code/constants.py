15c63da3e537d1dce3efa3a1087704b1af4e1989
tejas <tejas.morbagalharish@brockmann-consult.de>
2024-12-27 17:20:11 +0100

    updated constants

--- deep_code/constants.py
+++ deep_code/constants.py
@@ -1,4 +1,4 @@
 OSC_SCHEMA_URI = "https://stac-extensions.github.io/osc/v1.0.0-rc.3/schema.json"
 OSC_REPO_OWNER = "ESA-EarthCODE"
 OSC_REPO_NAME = "open-science-catalog-metadata-testing"
-OSC_NEW_BRANCH_NAME = "add-new-collection"
+OSC_BRANCH_NAME = "add-new-collection"
