e4fe7f2b0737d11d8d0785171ddc61d4424f1ea2
tejas <tejas.morbagalharish@brockmann-consult.de>
2024-12-27 16:17:43 +0100

    add main func for cli

---
+++ deep_code/cli/main.py
@@ -0,0 +1,12 @@
+import click
+
+from deep_code.cli.publish import publish_product
+
+@click.group()
+def main():
+    """Deep Code CLI."""
+    pass
+
+main.add_command(publish_product)
+if __name__ == "__main__":
+    main()
