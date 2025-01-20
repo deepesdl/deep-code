de3c504341a0eec28f24766ef83987a79cd0f441
tejas <tejas.morbagalharish@brockmann-consult.de>
2025-01-20 13:47:51 +0100

    code formatting

--- deep_code/utils/dataset_stac_generator.py
+++ deep_code/utils/dataset_stac_generator.py
@@ -4,8 +4,8 @@
 # Permissions are hereby granted under the terms of the MIT License:
 # https://opensource.org/licenses/MIT.
 
-import os
 import logging
+import os
 from datetime import datetime, timezone
 
 import pandas as pd
