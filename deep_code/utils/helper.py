1fa680c3f20b912b2e5d55b80f5bea3359ef1194
konstntokas <konstantin.ntokas@brockmann-consult.de>
2026-09-16 16:20:34 +0200

    file size added

--- deep_code/utils/helper.py
+++ deep_code/utils/helper.py
@@ -95,14 +95,10 @@ def open_dataset(
                 storage_options=config["params"]["storage_options"],
             )
             dataset = store.open_data(dataset_id)
-            print(dataset)
-            files = store.fs.find(dataset_id)
-            print(files)
+            files = store.fs.find(f"{store.root}/{dataset_id}")
             dataset.attrs["size"] = sum(store.fs.info(file)["size"] for file in files)
-            print(dataset.attrs["size"])
-            metadata_path = f"{dataset_id}/.zmetadata"
+            metadata_path = f"{store.root}/{dataset_id}/.zmetadata"
             dataset.attrs["metadata_size"] = store.fs.info(metadata_path)["size"]
-            print(dataset.attrs["metadata_size"])
             logger.info(
                 f"Successfully opened dataset '{dataset_id}' with configuration: "
                 f"{config['description']}"
